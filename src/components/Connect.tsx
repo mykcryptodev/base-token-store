ac6191d02ee7ea2e0715bee8ab0b7ee692ef074f
Myk <mykcryptodev@gmail.com>
2024-06-11 08:14:11 -0400

    app metadata

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -4,6 +4,7 @@ import { ConnectButton, useActiveAccount } from "thirdweb/react";
 import { client } from "~/providers/Thirdweb";
 import { createWallet } from "thirdweb/wallets";
 import { DEFAULT_CHAIN } from "~/constants/chain";
+import { APP_NAME } from "~/constants";
 
 export const Connect: FC = () => {
   const account = useActiveAccount();
@@ -23,6 +24,11 @@ export const Connect: FC = () => {
         client={client}
         chain={DEFAULT_CHAIN}
         wallets={[createWallet("com.coinbase.wallet")]}
+        appMetadata={{
+          name: APP_NAME,
+          description: "A marketplace for tokens and memes",
+          logoUrl: "https://avatars.githubusercontent.com/u/108554348?s=200&v=4"
+        }}
       />
       <Link href={`/profile/${account.address}`} className="btn btn-ghost flex h-10 items-center space-x-2">
         Portfolio
