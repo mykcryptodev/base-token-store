f26be4fd9ac90a81f867763e9f3dabe7f5afe7bb
Myk <mykcryptodev@gmail.com>
2024-06-11 08:16:53 -0400

    real cg daata

--- src/server/api/routers/kyberswap.ts
+++ src/server/api/routers/kyberswap.ts
@@ -65,17 +65,6 @@ const KyberswapRouteSummarySchema = z.object({
 
 const KYBER_BASE_URL = `https://aggregator-api.kyberswap.com`;
 
-const USDC = {
-  [1]: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-  [56]: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
-  [42161]: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
-  [137]: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
-  [10]: "0x0b2c639c533813f4aa9d7837caf62653d097ff85",
-  [43114]: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
-  [8453]: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
-  [324]: "0x1d17CBcF0D6D143135aE902365D2E5e2A16538D4"
-} as Record<string, string>;
-
 const CHAIN_NAME_MAP = {
   [1]: "ethereum",
   [56]: "bsc",
