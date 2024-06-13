4429aae6330f9acfef7002eeac02764adc61faf0
Myk <mykcryptodev@gmail.com>
2024-06-13 19:15:13 -0400

    design improvement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import TokenGrid from "~/components/Token/Grid";
+import { APP_NAME } from "~/constants";
 
 export default function Home() {
   return (
@@ -11,16 +12,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-2 px-4 py-16 ">
-          <h1 className="text-7xl tracking-tight flex items-center gap-2 sm:gap-4 flex-wrap sm:pb-8 pb-4 text-black">
-            <Image src="/images/logo-vertical.png" alt="Base Token Store" width={85} height={85} priority />
-            <div className="flex flex-col gap-4">
-              <span className="-mt-3">Base</span>
-              <span className="-mt-4">Token</span>
-              <span className="-mt-5">
-                Store
-              </span>
-            </div>
+        <div className="container flex flex-col items-center justify-center gap-2 px-4 pb-16 ">
+          <Image src="/images/logo-horizontal.svg" alt={APP_NAME} className="h-10 w-auto sm:hidden flex mt-8" width={200} height={200} priority />
+          <h1 className="sm:text-7xl text-5xl text-center tracking-tighter font-semibold items-center gap-2 sm:gap-4 flex-wrap sm:pt-8 pt-2 sm:pb-8 pb-4">
+            Base Token Store
           </h1>
           <TokenGrid />
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        inter: ['Inter', 'sans-serif'],
       },
     },
   },
@@ -18,6 +19,11 @@ export default {
           // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/dot-notation, @typescript-eslint/no-unsafe-member-access
           ...require("daisyui/src/theming/themes")["winter"],
           "primary": "#0052FF",
+          "primary-content": "#FFFFFF",
+          "base-content": "#011046",
+          "secondary": "#ECEFFA",
+          "secondary-content": "#011046",
+          "--rounded-btn": "9999px",
         },
       }
     ],
