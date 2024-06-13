52686619f870c55d2f0c2d32cf5399ccb753de9c
Myk <mykcryptodev@gmail.com>
2024-06-13 11:39:19 -0400

    og tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon.png"></link>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta property="og:title" content="Base Token Store" />
+        <meta property="og:description" content="The easiest way to buy any token on Base" />
+        <meta property="og:image" content="/images/lockup.webp" />
+        <meta property="og:url" content="https://basetokenstore.com" />
       </Head>
       <body className="font-brand">
         <Main />
