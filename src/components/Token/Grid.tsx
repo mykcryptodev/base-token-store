c25b43a39261bf2002636f24a4a60ff5e03d3b99
Myk <mykcryptodev@gmail.com>
2024-06-15 11:00:09 -0400

    show cat pic if no token found

--- src/components/Token/Grid.tsx
+++ src/components/Token/Grid.tsx
@@ -58,6 +58,79 @@ export const TokenGrid: FC = () => {
     return searchedTokens.filter((token) => !tokens?.find((t) => t.id === token.id));
   }, [tokens, searchedTokens]);
 
+  const TokenLoadingCard: FC = () => (
+    <div className="card w-60 bg-base-200 h-96 raise-on-hover cursor-pointer">
+      <div className="card-body p-4 animate-pulse">
+        <div className="flex items-center justify-between gap-2">
+          <div className="bg-base-300 w-14 rounded-full h-14" />
+          <div className="flex flex-col gap-2">
+            <div className="bg-base-300 w-20 rounded h-4" />
+            <div className="bg-base-300 w-20 rounded h-4" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="bg-base-300 w-32 rounded h-6" />
+          <div className="bg-base-300 w-20 rounded h-4" />
+        </div>
+        <div className="bg-base-300 w-full h-44 rounded-lg" />
+        <div className="bg-base-300 w-28 rounded-full h-12" />
+      </div>
+    </div>
+  );
+
+  const TokenNotFound: FC = () => {
+    const { data: catPic } = api.cats.getCatPic.useQuery(undefined, {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    });
+    return (
+      <div className="flex flex-col w-full items-center justify-center gap-2">
+        <div className="card max-w-xs bg-base-200 text-center justify-center flex raise-on-hover cursor-pointer">
+          <div className="card-body p-4">
+            <div className="flex w-full flex-col gap-2 justify-center">
+              <div className="w-full flex items-center justify-center">
+                <div className="bg-warning w-14 rounded-full h-14 flex items-center justify-center">
+                  <ExclamationTriangleIcon className="h-8 w-8 stroke-2" />
+                </div>
+              </div>
+              <div className="flex flex-col grow">
+                <span className="text-lg font-bold">No tokens found</span>
+                <span className="text-sm">Try another search</span>
+                <span className="text-sm mt-4 flex">
+                  <span>
+                    This empty search result feature and cat picture is brought to you by
+                    🐾 <Link href="https://pawthereum.com" className="underline">Pawthereum</Link>
+                  </span>
+                </span>
+                <span className="text-xs mt-4 flex">
+                  <span>
+                    Your token community can <Link href="https://github.com/mykcryptodev/base-token-store" className="underline">contribute features</Link> on Base Token Store to gain exposure!
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex sm:flex-row flex-col items-center gap-2">
+          <div className="flex justify-center">
+            {!fallbackToken ? (
+              <TokenLoadingCard />
+            ) : (
+              <TokenCard key={fallbackToken.id} token={fallbackToken} />
+            )}
+          </div>
+          {catPic ? (
+            <div className={`card max-w-[236px] min-h-[300px] bg-base-100 raise-on-hover cursor-pointer overflow-hidden`}>
+              <Image src={catPic} alt="Cat says: Oh noez! No token found!" width={300} height={300} className="rounded-lg" />
+            </div>
+          ) : (
+            <TokenLoadingCard />
+          )}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
       <div className="flex justify-center w-full">
@@ -90,55 +163,11 @@ export const TokenGrid: FC = () => {
             {filteredTokens?.map((token) => <TokenCard key={token.id} token={token as TokenListResponse} />)}
             {searchedTokensNotInCategory?.map((token) => <TokenCard key={token.id} token={token} />)}
             {(tokensIsLoading || searchIsLoading) && Array.from({ length: tokensPerPage }, (_, index) => (
-              <div key={index} className="card w-60 bg-base-200 h-96 raise-on-hover cursor-pointer">
-                <div className="card-body p-4 animate-pulse">
-                  <div className="flex items-center justify-between gap-2">
-                    <div className="bg-base-300 w-14 rounded-full h-14" />
-                    <div className="flex flex-col gap-2">
-                      <div className="bg-base-300 w-20 rounded h-4" />
-                      <div className="bg-base-300 w-20 rounded h-4" />
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <div className="bg-base-300 w-32 rounded h-6" />
-                    <div className="bg-base-300 w-20 rounded h-4" />
-                  </div>
-                  <div className="bg-base-300 w-full h-44 rounded-lg" />
-                  <div className="bg-base-300 w-28 rounded-full h-12" />
-                </div>
-              </div>
+              <TokenLoadingCard key={index} />
             ))}
           </div>
           {!tokensIsLoading && !searchIsLoading && filteredTokens?.length === 0 && searchedTokensNotInCategory?.length === 0 && fallbackToken && (
-            <div className="flex flex-col w-full items-center justify-center gap-2">
-              <div className="card max-w-xs bg-base-200 text-center justify-center flex raise-on-hover cursor-pointer">
-                <div className="card-body p-4">
-                  <div className="flex w-full flex-col gap-2 justify-center">
-                    <div className="w-full flex items-center justify-center">
-                      <div className="bg-warning w-14 rounded-full h-14 flex items-center justify-center">
-                        <ExclamationTriangleIcon className="h-8 w-8 stroke-2" />
-                      </div>
-                    </div>
-                    <div className="flex flex-col grow">
-                      <span className="text-lg font-bold">No tokens found</span>
-                      <span className="text-sm">Try another search</span>
-                      <span className="text-sm mt-4 flex">
-                        <span>
-                          This empty search result feature is brought to you by
-                          🐾 <Link href="https://pawthereum.com" className="underline">Pawthereum</Link>
-                        </span>
-                      </span>
-                      <span className="text-xs mt-4 flex">
-                        <span>
-                          Your token community can <Link href="https://github.com/mykcryptodev/base-token-store" className="underline">contribute features</Link> on Base Token Store to gain exposure!
-                        </span>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <TokenCard key={fallbackToken.id} token={fallbackToken} />
-            </div>
+            <TokenNotFound />
           )}
           <div className="flex w-full justify-end">
             <Link href="https://coingecko.com" target="_blank" rel="noopener noreferrer" className="flex gap-1 items-center">
---
+++ src/server/api/routers/cats.ts
@@ -0,0 +1,50 @@
+import fetch from "node-fetch";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const catsRouter = createTRPCRouter({
+  getCatPic: publicProcedure
+    .query(async () => {
+      const textOptions = [
+        "Henlo",
+        "Meow",
+        "do me a pet",
+        "hai fren",
+        "hai",
+        "Oh noez!",
+        "I can haz cheezburger?",
+        "I can haz base token?",
+        "wen salami?",
+        "wen moon?",
+        "wen lambo?",
+        "wen catnip?",
+      ] as const;
+      const randomText = textOptions[Math.floor(Math.random() * textOptions.length)];
+      const urlEncodedText = encodeURIComponent(randomText!);
+      const params = new URLSearchParams({
+        font: "Impact",
+        fontSize: "72",
+        fontColor: "#FFFFFF",
+        fontBackground: "#000000",
+        position: "center",
+      }).toString();
+    
+      const catRes = await fetch(`https://cataas.com/cat/says/${urlEncodedText}?${params}`);
+      console.log(`
+      
+      
+      
+      === CAT PIC ===
+      
+      
+      
+      `)
+      console.log(catRes);
+      const catArrayBuffer = await catRes.arrayBuffer();
+      const catImage = Buffer.from(catArrayBuffer);
+      return `data:${catRes.headers.get('content-type')};base64,${catImage.toString('base64')}`;
+    }),
+});
