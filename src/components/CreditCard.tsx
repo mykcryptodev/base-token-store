16c760672cfef04273694b135dac3476758651c5
Myk <mykcryptodev@gmail.com>
2024-06-23 19:14:37 -0400

    boomer branding

--- src/components/CreditCard.tsx
+++ src/components/CreditCard.tsx
@@ -3,6 +3,8 @@ import { useTheme } from "next-themes";
 import { type FC } from "react";
 import { PayEmbed } from "thirdweb/react";
 import { client } from "~/providers/Thirdweb";
+import Image from 'next/image';
+import Link from "next/link";
 
 export const CreditCard: FC = () => {
   const { theme } = useTheme();
@@ -22,6 +24,44 @@ export const CreditCard: FC = () => {
           </label>
         </div>
         <h3 className="text-2xl font-bold mb-4 p-4 sm:p-0">Buy crypto with credit card</h3>
+        <div className="flex items-start gap-1">
+          <div className="min-w-[200px]">
+            <Image
+              src="/images/boomer-logo.svg"
+              alt="boomer"
+              width={250}
+              height={250}
+              className="mx-auto"
+            />
+            <p className="prose pl-2 text-xs pt-2">
+              {`Sup, Boomer! If you don't already have crypto, you can buy some with your credit card right here!`}
+            </p>
+            <p className="prose pl-2 text-xs py-2">
+              {`It only takes a few minutes to move funds onchain. So get going and welcome to Base!`}
+            </p>
+            <Image
+              src="/images/boomer-icon.webp"
+              alt="boomer"
+              width={100}
+              height={100}
+              className="rounded-full mx-2 float-left w-12 h-12"
+            />
+            <p className="prose pl-2 text-xs py-2">
+              Brought to you by{" "}
+              <Link href="https://www.baseboomer.com/" className="underline" target="_blank" rel="noreferrer">
+                Boomer
+              </Link>
+            </p>
+
+          </div>
+          <Image
+            src="/images/boomer.webp"
+            alt="boomer"
+            width={250}
+            height={250}
+            className="mx-auto"
+          />
+        </div>
         <PayEmbed
           client={client}
           theme={theme === "dark" ? "dark" : "light"}
@@ -34,6 +74,15 @@ export const CreditCard: FC = () => {
             buyWithCrypto: false,
           }}
         />
+        <div className="absolute bottom-4 left-4 -z-10">
+          <Image
+            src="/images/boomer-lawn.webp"
+            alt="boomer"
+            width={50}
+            height={50}
+            className="mx-auto"
+          />
+        </div>
         <div className="modal-action">
           <label htmlFor="credit-card-modal" className="btn">Close</label>
         </div>
